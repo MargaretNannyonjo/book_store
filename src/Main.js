3c624607e09dfb586e151cf95af7c3ca2cc12f49
MargaretNannyonjo <nannyonjoleon@gmail.com>
2024-01-27 01:43:00 +0300

    book_store

---
+++ src/Main.js
@@ -0,0 +1,84 @@
+import React, { useState } from "react";
+import readingImage from "./images/reading.avif";
+import shelfImage from "./images/shelf.jpg";
+import { Card } from "./Card";
+import axios from "axios";
+
+export const Main = () => {
+  const [search, setSearch] = useState("");
+  const [bookData, setData] = useState([]);
+  const searchBook = (evt) => {
+    if (evt.key === "Enter") {
+      axios
+        .get(
+          "https://www.googleapis.com/books/v1/volumes?q=" +
+            search +
+            "&key=AIzaSyCLfvnrmXt0flzKsluUs1F4yC0dHJUrOrA" +
+            "&maxResults=40"
+        )
+        .then((res) => setData(res.data.items))
+        .catch((err) => console.log(err));
+    }
+  };
+
+  const backgroundStyle = {
+    width: "100%",
+    height: "500px",
+    color: "#d48996ff",
+    boxShadow: "0 5px 10px #d48996ff",
+    display: "flex",
+    alignContent: "center",
+    position: "relative",
+  };
+
+  return (
+    <>
+      <div className="header" style={backgroundStyle}>
+        <div
+          className="row1"
+          style={{
+            content: "''",
+            position: "absolute",
+            top: 0,
+            left: 0,
+            width: "100%",
+            height: "100%",
+            background: `linear-gradient(rgba(0, 0, 0, 0.5), rgba(0, 0, 0, 0.5)),url(${shelfImage})`,
+            clipPath: "circle(65% at left 300px)",
+            zIndex: -1,
+          }}
+        >
+          <h1>
+            Dive into the Pages of infinite Wonder,
+            <br />
+            <span style={{ paddingLeft: "20px" }}>
+              Your Next Favorite Story Awaits
+            </span>
+          </h1>
+        </div>
+
+        <div className="row2">
+          <h2>THE BOOK SHELF</h2>
+          <div className="search">
+            <input
+              type="text"
+              placeholder="Enter a Book Name"
+              value={search}
+              onChange={(e) => setSearch(e.target.value)}
+              onKeyDown={searchBook}
+            />
+            <button
+              onClick={() => searchBook({ key: "Enter" })}
+              style={{ cursor: "pointer" }}
+            >
+              <i className="fas fa-search"></i>
+            </button>
+          </div>
+          <img src={readingImage} alt="" />
+        </div>
+      </div>
+
+      <div className="container">{<Card book={bookData} />}</div>
+    </>
+  );
+};
